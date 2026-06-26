b3925f235d10aee315d7f730d2f80808fcb72f71
jay6rathod <jay.rathod@impactguru.com>
2026-06-26 17:58:16 +0530

    Completed the right side content of the page

--- scripts/page_script/script.js
+++ scripts/page_script/script.js
@@ -1,2 +1,18 @@
 $("#nav").load('../../components/navbar.html')
 $("#carousel-content").load('../../components/carousels.html')
+
+
+// the script for read more button
+function toggleReadMore() {
+    const hiddenPart = document.querySelector('.hidden_part');
+    const read_more_btn = document.querySelector('.read-more-btn1');
+
+    if(hiddenPart.style.display === "block"){
+        hiddenPart.style.display = "none";
+        read_more_btn.style.display = "block"
+    }
+    else{
+        hiddenPart.style.display = "block";
+        read_more_btn.style.display = "none"
+    }
+}
